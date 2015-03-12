3bf55ce0a306142fce338b005160edef532803ec
Samy Pessé <samypesse@gmail.com>
2015-03-12 18:20:20 +0100

    Add passive checker

--- test/passive.js
+++ test/passive.js
@@ -1,12 +1,12 @@
 var should = require("should");
 var rousseau = require("../lib")
 
-describe("So", function() {
+describe("Passive", function() {
     it("should fail for non-existant language", function(done) {
-        rousseau("So the cat was stole.", function(err, results) {
+        rousseau("He was judged.", function(err, results) {
             should.not.exist(err);
             results.should.have.length(1);
-            should(results[0].type).be.exactly("so");
+            should(results[0].type).be.exactly("passive");
             done();
         });
     });
