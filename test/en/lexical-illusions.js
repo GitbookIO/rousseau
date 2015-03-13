52ed03542f4ba6c7aa293305ece67a96a72d71ae
Samy Pessé <samypesse@gmail.com>
2015-03-13 12:08:12 +0100

    Improve lexical-illusion and improve test

--- test/en/lexical-illusions.js
+++ test/en/lexical-illusions.js
@@ -1,9 +1,15 @@
 describe("Lexical Illusions", function() {
-    var results = rousseau("the the");
+
 
     it("should detect", function() {
+        var results = rousseau("the the", { only: ["lexical-illusion"] });
         results.should.have.length(1);
         results[0].type.should.be.exactly("lexical-illusion");
         results[0].index.should.be.exactly(4);
+    });
+
+    it("should split correctly sentences", function() {
+        var results = rousseau("offer to sell, sell", { only: ["lexical-illusion"] });
+        results.should.have.length(0);
     });
 });
