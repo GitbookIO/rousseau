3dc83e6f12cf0e902ceec12240c6df2edb88f5c8
Samy Pessé <samypesse@gmail.com>
2015-03-16 17:54:53 +0100

    Fix command line utility

--- bin/rousseau.js
+++ bin/rousseau.js
@@ -11,8 +11,10 @@ var rousseau = require('../lib');
 
 // Colors for levels
 var LEVELS = {
+    critical: color.red,
     error: color.red,
-    warn: color.yellow
+    warning: color.yellow,
+    suggestion: color.cyan
 };
 
 
@@ -29,31 +31,40 @@ if (!input) {
 // Read and lint file
 var content = fs.readFileSync(input, { encoding: "utf-8" });
 
+rousseau(content, function(err, results) {
+    if (err) {
+        console.log(color.red("Error: "+(err.stack || err)));
+        return process.exit(1);
+    }
 
-var results = rousseau(content);
-var levels = {};
-var table = new Table({
-    chars: { 'top': '' , 'top-mid': '' , 'top-left': '' , 'top-right': ''
-     , 'bottom': '' , 'bottom-mid': '' , 'bottom-left': '' , 'bottom-right': ''
-     , 'left': '' , 'left-mid': '' , 'mid': '' , 'mid-mid': ''
-     , 'right': '' , 'right-mid': '' , 'middle': ' ' },
-    style: { 'padding-left': 0, 'padding-right': 0 }
-});
-
-if (results.length > 0) {
-    console.log("Results for '"+color.wrap(input, "white", "underline")+"':");
-    console.log("");
-
-    _.each(results, function(result) {
-        var color = LEVELS[result.level];
-        levels[result.level] = (levels[result.level] || 0) + 1;
-
-        table.push(["  ", result.index+":"+result.offset, color("["+result.level+"]"), result.message]);
+    var levels = {};
+    var table = new Table({
+        chars: { 'top': '' , 'top-mid': '' , 'top-left': '' , 'top-right': ''
+         , 'bottom': '' , 'bottom-mid': '' , 'bottom-left': '' , 'bottom-right': ''
+         , 'left': '' , 'left-mid': '' , 'mid': '' , 'mid-mid': ''
+         , 'right': '' , 'right-mid': '' , 'middle': ' ' },
+        style: { 'padding-left': 0, 'padding-right': 0 }
     });
 
-    console.log(table.toString());
-    console.log("");
-    console.log(color.cyan(results.length+" Problems ("+levels.error+" errors, "+levels.warn+" warnings)"));
-} else {
-    console.log(COLOR_GREEN+"All good!"+COLOR_END)
-}
+    if (results.length > 0) {
+        console.log("Results for '"+color.wrap(input, "white", "underline")+"':");
+        console.log("");
+
+        _.each(results, function(result) {
+            var color = LEVELS[result.level];
+            levels[result.level] = (levels[result.level] || 0) + 1;
+
+            table.push([
+                    "  ", result.index+":"+result.offset,
+                    color("["+result.level+"]"),
+                    result.message
+            ]);
+        });
+
+        console.log(table.toString());
+        console.log("");
+        console.log(color.cyan(results.length+" Problems ("+(levels.error || 0)+" errors, "+(levels.warning || 0)+" warnings)"));
+    } else {
+        console.log(COLOR_GREEN+"All good!"+COLOR_END)
+    }
+});
